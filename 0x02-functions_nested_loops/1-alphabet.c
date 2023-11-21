c1212c5239f39534180c88ef3ed8c091642f040b
mohamednaguib305 <mohamednaguib305@gmail.com>
2023-11-21 00:40:09 -0800

    up

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,10 @@
 void print_alphabet(void)
 {
 	char alpha;
-	for(alpha= 'a';alpha<= 'z'; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-	_putchar(alpha);
+		_putchar(alpha);
 	}
 	_putchar('\n');
 }
