ba20eb6afcf38d1223f01370e976006c8be9f141
mohamednaguib305 <mohamednaguib305@gmail.com>
2023-11-21 00:53:18 -0800

    up

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char alpha;
 	int count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
